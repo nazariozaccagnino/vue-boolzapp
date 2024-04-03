219ce0a489db43198c5baca53e8f84088e454e5c
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-04-03 10:08:10 +0200

    initial commit of files

---
+++ js/script.js
@@ -0,0 +1,20 @@
+import { contacts } from "./data.js";
+
+const {createApp} = Vue;
+
+createApp({
+    data(){
+        return{
+            contacts
+        }
+    },
+    methods:{
+
+    },
+    computed:{
+
+    },
+    mounted(){
+        console.log(this.contacts);
+    }
+}).mount('#app')
