d289c5dd88ca08364cf3e37a72385397ba8f67d1
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-04-04 15:30:01 +0200

    big commit after lesson

--- js/script.js
+++ js/script.js
@@ -1,24 +1,40 @@
 import { contacts } from "./data.js";
 
+const dt = luxon.DateTime
 const {createApp} = Vue;
 
 createApp({
     data(){
         return{
             contacts: contacts,
-            activeContactId: 0,
+            activeContactId: 1,
+            messageText: '',
+            searchText: '',
         }
     },
     methods:{
         setActiveChat(id){
-            this.activeContactId = id + 1
-        }
-
+            this.activeContactId = id
+        },
+        sendMessage(){
+            const newMessage = {
+                date: dt.now().setLocale('it').toFormat('dd/MM/yyyy HH:mm:ss'),
+                message: this.messageText,
+                status: 'sent',
+            }
+            this.activeContact.messages.push(newMessage)
+            this.messageText = '';
+        },
     },
     computed:{
-
+        activeContact(){
+            return this.contacts.find((el)=> el.id === this.activeContactId)
+        },
+        filteredContacts(){
+            return this.contacts.filter((el)=> el.name.toLowerCase().includes(this.searchText.toLowerCase()))
+        }
     },
     mounted(){
         // console.log(this.contacts);
     }
-}).mount('#app')
+}).mount('#app');
